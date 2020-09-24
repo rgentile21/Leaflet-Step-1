7dca7e6da617b821aba8eafed6bb6f5cef20d8f8
Richard Gentile <mgentile@wideopenwest.com>
2020-09-24 18:35:39 -0400

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,16 @@
 // API key
-var API_KEY = "pk.eyJ1IjoicmdlbnRpbGUyMSIsImEiOiJja2VmcmJqbjUwYXk0MnVvNGR1eG03dWZ2In0.zA514adLtYATMKZHS-qd6Q";
+var API_KEY = "pk.eyJ1IjoicmdlbnRpbGUyMSIsImEiOiJja2VmcmJqbjUwYXk0MnVvNGR1eG03dWZ2In0.zA514adLtYATMKZHS-qd6Q";.
+
+var attribution = `
+  Map data & copy;
+  <a href="https://www.openstreetmap.org/">
+    OpenStreetMap
+    </a>
+  contributors,
+    <a href="https://creativecommons.org/licenses/by-sa/2.0/">
+      CC-BY-SA
+    </a>
+    , Imagery ©
+  <a href="https://www.mapbox.com/">
+    Mapbox
+    </a>`;
