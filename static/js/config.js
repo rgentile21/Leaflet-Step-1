2ad6b6ceb3c7ad8cbb244f45c8fe342786553e30
Richard Gentile <mgentile@wideopenwest.com>
2020-09-24 19:06:07 -0400

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 // API key
-var API_KEY = "pk.eyJ1IjoicmdlbnRpbGUyMSIsImEiOiJja2VmcmJqbjUwYXk0MnVvNGR1eG03dWZ2In0.zA514adLtYATMKZHS-qd6Q";.
+var API_KEY = "";.
 
 var attribution = `
   Map data & copy;
